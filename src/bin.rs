b79e956ea9e505dceeb3098fb50a604e0fe61733
Filip Paulů <ing.fenix@seznam.cz>
2020-10-09 12:45:39 +0200

    Remove unnecesary or values

--- src/bin.rs
+++ src/bin.rs
@@ -57,36 +57,28 @@ fn main() {
 
     let file_path = matches.value_of("input").unwrap();
 
-    let time_interval = match matches
-        .value_of("time_interval")
-        .unwrap_or("1000")
-        .parse::<usize>()
-    {
+    let time_interval = match matches.value_of("time_interval").unwrap().parse::<usize>() {
         Ok(n) => n,
         Err(_) => panic!("The time_interval have to be integer."),
     };
     let analysis_interval = match matches
         .value_of("analysis_interval")
-        .unwrap_or("3000")
+        .unwrap()
         .parse::<usize>()
     {
         Ok(n) => n,
         Err(_) => panic!("The analysis_interval have to be integer."),
     };
-    let min_bpm = match matches.value_of("min_bpm").unwrap_or("80").parse::<usize>() {
+    let min_bpm = match matches.value_of("min_bpm").unwrap().parse::<usize>() {
         Ok(n) => n,
         Err(_) => panic!("The min_bpm have to be integer."),
     };
-    let max_bpm = match matches
-        .value_of("max_bpm")
-        .unwrap_or("160")
-        .parse::<usize>()
-    {
+    let max_bpm = match matches.value_of("max_bpm").unwrap().parse::<usize>() {
         Ok(n) => n,
         Err(_) => panic!("The max_bpm have to be integer."),
     };
 
-    let verbose = match matches.value_of("verbose").unwrap_or("1") {
+    let verbose = match matches.value_of("verbose").unwrap() {
         "0" => 0,
         "1" => 1,
         _ => panic!("Unknown verbose. allowed values are \"0\" and \"1\"."),
